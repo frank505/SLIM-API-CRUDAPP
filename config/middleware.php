a7aa625b0230d411865eb3585f56263933c867cf
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2020-05-12 08:29:34 -0700

    init commit

---
+++ config/middleware.php
@@ -0,0 +1,16 @@
+<?php
+
+use Selective\Config\Configuration;
+use Slim\App;
+use Slim\Middleware\ErrorMiddleware;
+
+return function (App $app) {
+    // Parse json, form data and xml
+    $app->addBodyParsingMiddleware();
+
+    // Add the Slim built-in routing middleware
+    $app->addRoutingMiddleware();
+
+    // Catch exceptions and errors
+    $app->add(ErrorMiddleware::class);
+};
---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\App;
+
+return function (App $app) {
+   
+    $app->get('/', \App\Action\HomeAction::class);
+};
